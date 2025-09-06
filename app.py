498320210b5605d6175c84bc739b9527796f4ca0
Zade <muhderfannnn@gmail.com>
2025-09-07 01:43:21 +0800

    Updated app.py to include some page configs. - Zade

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="BullBearAnalysis",
+    page_icon="📈",
+    layout="wide"
+)
+
+
+# UIUX should go here into app.py
+
 
 st.title("Stock Analysis Project")
-st.write("Hello World")
+st.write("Hello World!")
